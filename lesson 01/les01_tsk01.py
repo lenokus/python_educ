2f08915a358207d34626eea12662623828cea4c3
ustyugova lena <my_dustbin@mail.ru>
2021-10-13 20:45:17 +0300

    lesson 01 (#1)

---
+++ lesson 01/les01_tsk01.py
@@ -0,0 +1,13 @@
+#1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел
+# и строк и сохраните в переменные, выведите на экран.
+
+a = input('Please, enter any number: ')
+b = input('Please, enter any number: ')
+
+print ('a + b = ', int(a) + int(b))
+print ('a - b = ', int(a) - int(b))
+print ('a * b = ', int(a) * int(b))
+print ('a / b = ', int(a) / int(b))
+print ('a // b = ', int(a) // int(b))
+print ('a % b = ', int(a) % int(b))
+print ('a ** b = ', int(a) ** int(b))
